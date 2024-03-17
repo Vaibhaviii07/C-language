e8502476cf60cd9cffe2ef0a5f704bef64e251d6
Vaibhavi Tingane <123562096+Vaibhaviii07@users.noreply.github.com>
2024-03-17 15:50:55 +0530

    Add files via upload

---
+++ Assigment1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    int Perimeter,Area,Height=7,Width=5;
+    printf("perimeter of rectangle  = %d inches\n",(2*Height + 2*Width));
+    printf("Area of rectangle = %d square inches\n",Height*Width);
+    return 0;
+}
