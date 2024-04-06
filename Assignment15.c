087ccf3046ab91237b71bcf46cd3a24f01be84c0
Vaibhavi Tingane <123562096+Vaibhaviii07@users.noreply.github.com>
2024-04-06 22:11:06 +0530

    Add files via upload

---
+++ Assignment15.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+int main()
+{
+   int i,j,num=0;
+   int a[3][3];
+   int b[3][3];
+   int c[3][3];
+
+   //FIRST
+   printf("Enter even numbers only\n"); 
+   for(i=0;i<3;i++)
+   {
+        for(j=0;j<3;j++)
+        {   
+             
+            while(1)
+            {  
+                
+                printf("Enter a[%d][%d]=",i,j);
+                scanf("%d",&num);
+                if(num%2 == 0)
+                {
+                    a[i][j]=num;
+                    break;
+                }
+            }
+        }
+   }
+    printf("\n\n");
+//SECOND ARRAY
+printf("Enter odd numbers only\n"); 
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            while(1)
+            {
+                printf("Enter b[%d][%d]=",i,j);
+                scanf("%d",&num);
+                if(num%2!=0)
+                {
+                    b[i][j]=num;
+                    break;
+                }  
+            }
+        }   
+    }  
+
+
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d",a[i][j]);
+        }  
+        printf("\n");
+    }  
+    printf("\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d",b[i][j]);
+        }    
+        printf("\n");
+    }   
+    printf("\n");
+
+//R E S U L T
+
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            c[i][j]=a[i][j] + b[i][j];
+            printf("%d ",c[i][j]);
+        }    
+        printf("\n");    
+    }
+}
